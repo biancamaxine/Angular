80efdd31b4a5c239693217f517df2ce2f6f479d6
biancamaxine <bianca.maxine.dev@gmail.com>
2023-03-21 01:52:59 -0400

    Start New Project: No Name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'welcome' },
+  {
+    path: 'welcome',
+    loadChildren: () => import('./views/views.module').then(m => m.ViewsModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./views/auth/auth.module').then(m => m.AuthModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
